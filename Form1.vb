11aa58a5c629af91083dd573e73b95dd48fde961
RAJ2410-patel <patel2410raj@gmail.com>
2026-02-25 07:40:29 +0530

    Creating Form1.vb for gym equipment selling system

---
+++ Form1.vb
@@ -0,0 +1,16 @@
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Gym Equipment Selling System"
+        InitializeComponent()
+    End Sub
+
+    Private Sub ButtonSell_Click(sender As Object, e As EventArgs) Handles ButtonSell.Click
+        ' Code for selling equipment goes here
+    End Sub
+
+    Private Sub ButtonStock_Click(sender As Object, e As EventArgs) Handles ButtonStock.Click
+        ' Code for managing stock goes here
+    End Sub
+
+    ' Additional methods for the form can be added here
+End Class
